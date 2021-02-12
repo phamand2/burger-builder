e3265e9d9f2098a12b6cd81cfd142da4d5393f56
Andrew Pham <andrewpham92@gmail.com>
2021-02-12 13:16:23 -0500

    added dynamic ingredient component

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from './Layout.module.css'
 
 const Layout = (props) => {
   return (
     <>
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>{props.children}</main>
+      <main className={styles.Content}>{props.children}</main>
     </>
   );
 };
