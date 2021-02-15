e13ddfc38508abc8a4a9df1900e08742fe2e9a77
Andrew Pham <andrewpham92@gmail.com>
2021-02-15 14:29:13 -0500

    added button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../../UI/Button/Button";
 
 const OrderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -16,6 +17,8 @@ const OrderSummary = (props) => {
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <Button clicked={props.purchaseCanceled}  btnType='Danger'>CANCEL</Button>
+      <Button clicked={props.purchaseContinue}  btnType='Success'>CONTINUE</Button>
     </>
   );
 };
