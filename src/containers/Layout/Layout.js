99565e03fe1400f6c735ceec8b6095c390b687d0
Andrew Pham <andrewpham92@gmail.com>
2021-02-15 15:11:51 -0500

    added toolbar

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import styles from './Layout.module.css'
 
 const Layout = (props) => {
   return (
     <>
-      <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar />
       <main className={styles.Content}>{props.children}</main>
     </>
   );
