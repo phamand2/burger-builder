4648ce1f4bbd34bc4c5884c010a9f07988285cf9
Andrew Pham <andrewpham92@gmail.com>
2021-02-15 11:52:50 -0500

    Remove ingredients button

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,28 @@
+import React from "react";
+import BuildControl from "./BuildControl/BuildControl";
+import styles from "./BuildControls.module.css";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
+
+const BuildControls = (props) => {
+  return (
+    <div className={styles.BuildControls}>
+      {controls.map((ctrl) => (
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemove(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default BuildControls;
